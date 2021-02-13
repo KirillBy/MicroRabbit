599bd494535342b7a863e83f3c27085a0b6123b5
KirillBy <balanovichks@gmail.com>
2021-02-13 07:46:20 +0300

    Implemented interfaces interfaces and classes for Domain.Core

---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        public Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
